2dce098247619a986679aae6fac4e406e3e1dc7a
Md Imrul Hassan <mihassan@gmail.com>
2024-07-10 12:32:15 +1000

    Add a Debug module to convert back and forth from readable whitespace program.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,13 @@
 module Main where
 
+import Whitespace.DebugSpec qualified
 import Common.UtilSpec qualified
 import Test.Hspec
 
 spec :: Spec
 spec = do
   describe "Common.Util" Common.UtilSpec.spec
+  describe "Whitespace.Debug" Whitespace.DebugSpec.spec
 
 main :: IO ()
 main = hspec spec
--- test/common/UtilSpec.hs
+++ test/common/UtilSpec.hs
@@ -1,49 +1,48 @@
 module Common.UtilSpec where
 
 import Common.Util
-import Test.Hspec
+import SpecHelper
 
 spec :: Spec
 spec = do
-  describe "Common.Util" $ do
-    describe "unique" $ do
-      it "works on empty list" $ do
-        unique ([] :: [Int]) `shouldBe` True
-      it "works on single element list" $ do
-        unique [1 :: Int] `shouldBe` True
-      it "can detect duplicates" $ do
-        unique [1 :: Int, 1] `shouldBe` False
-      it "works on multiple element list" $ do
-        unique [1 :: Int, 2, 3] `shouldBe` True
-      it "can detect duplicates in multiple element" $ do
-        unique [1 :: Int, 2, 1] `shouldBe` False
+  describe "unique" $ do
+    it "works on empty list" $ do
+      unique ([] :: [Int]) `shouldBe` True
+    it "works on single element list" $ do
+      unique [1 :: Int] `shouldBe` True
+    it "can detect duplicates" $ do
+      unique [1 :: Int, 1] `shouldBe` False
+    it "works on multiple element list" $ do
+      unique [1 :: Int, 2, 3] `shouldBe` True
+    it "can detect duplicates in multiple element" $ do
+      unique [1 :: Int, 2, 1] `shouldBe` False
 
-    describe "maybeToEither" $ do
-      it "Just value is mapped to Right" $ do
-        maybeToEither "error" (Just 5 :: Maybe Int) `shouldBe` Right 5
-      it "Nothing is mapped to Left" $ do
-        maybeToEither "error" (Nothing :: Maybe Int) `shouldBe` Left "error"
+  describe "maybeToEither" $ do
+    it "Just value is mapped to Right" $ do
+      maybeToEither "error" (Just 5 :: Maybe Int) `shouldBe` Right 5
+    it "Nothing is mapped to Left" $ do
+      maybeToEither "error" (Nothing :: Maybe Int) `shouldBe` Left "error"
 
-    describe "guardE" $ do
-      it "When predicate is True, value is mapped to Right" $ do
-        guardE True "error" (5 :: Int) `shouldBe` Right 5
-      it "When predicate is False, error is mapped to Left" $ do
-        guardE False "error" (5 :: Int) `shouldBe` Left "error"
+  describe "guardE" $ do
+    it "When predicate is True, value is mapped to Right" $ do
+      guardE True "error" (5 :: Int) `shouldBe` Right 5
+    it "When predicate is False, error is mapped to Left" $ do
+      guardE False "error" (5 :: Int) `shouldBe` Left "error"
 
-    describe "(|>)" $ do
-      it "Can be used to apply an Int to a function" $ do
-        (5 |> (+ 3)) `shouldBe` (8 :: Int)
-      it "Can be used to apply a String to a function" $ do
-        ("hello" |> length) `shouldBe` (5 :: Int)
-      it "Can be chained from left to right" $ do
-        ("Hello" |> length |> odd) `shouldBe` True
+  describe "(|>)" $ do
+    it "Can be used to apply an Int to a function" $ do
+      (5 |> (+ 3)) `shouldBe` (8 :: Int)
+    it "Can be used to apply a String to a function" $ do
+      ("hello" |> length) `shouldBe` (5 :: Int)
+    it "Can be chained from left to right" $ do
+      ("Hello" |> length |> odd) `shouldBe` True
 
-    describe "(|>>)" $ do
-      it "Can be used to apply a function to a Maybe value" $ do
-        (Just 5 |>> (+ 3)) `shouldBe` Just (8 :: Int)
-      it "Can be used to apply a function to a List" $ do
-        ([1, 2, 3] |>> (+ 3)) `shouldBe` [4, 5, 6 :: Int]
-      it "Can be chained from left to right" $ do
-        (Just 5 |>> (+ 3) |>> (* 2)) `shouldBe` Just (16 :: Int)
-      it "Can be mixed with (|>)" $ do
-        ([1 .. 10] |>> (* 10) |> sum) `shouldBe` (550 :: Int)
+  describe "(|>>)" $ do
+    it "Can be used to apply a function to a Maybe value" $ do
+      (Just 5 |>> (+ 3)) `shouldBe` Just (8 :: Int)
+    it "Can be used to apply a function to a List" $ do
+      ([1, 2, 3] |>> (+ 3)) `shouldBe` [4, 5, 6 :: Int]
+    it "Can be chained from left to right" $ do
+      (Just 5 |>> (+ 3) |>> (* 2)) `shouldBe` Just (16 :: Int)
+    it "Can be mixed with (|>)" $ do
+      ([1 .. 10] |>> (* 10) |> sum) `shouldBe` (550 :: Int)
