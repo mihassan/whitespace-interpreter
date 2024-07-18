e3749cdd35908c876d0921c70a6df465ce50db9f
Md Imrul Hassan <mihassan@gmail.com>
2024-07-18 12:54:31 +1000

    Use optparse-applicative for argument parsing.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,30 @@
 module Main where
 
-import Whitespace
-import System.Environment
+import Options.Applicative
 import System.IO
+import Whitespace
+
+data Opts = Opts {file :: String}
+
+optsParser :: ParserInfo Opts
+optsParser =
+  info
+    (helper <*> versionOption <*> programOptions)
+    ( fullDesc
+        <> progDesc "optparse example"
+        <> header
+          "optparse-example - a small example program for optparse-applicative"
+    )
+  where
+    versionOption :: Parser (a -> a)
+    versionOption = infoOption "0.1.0.0" (long "version" <> help "Show version")
+    programOptions :: Parser Opts
+    programOptions = Opts <$> strOption (short 'f' <> long "file" <> metavar "FILE" <> help "File to run")
 
 main :: IO ()
 main = do
-  [f] <- getArgs
-  code <- readFile f
+  opts <- execParser optsParser
+  code <- readFile $ file opts
   input <- getContents
   case whitespace code input of
     Left err -> hPutStrLn stderr $ "Error running code: " <> err
